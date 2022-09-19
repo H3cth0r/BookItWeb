2e32c419019cefd9a5541eb47960bbff332a32b3
h3cth0r <hectorsucre13@gmail.com>
2022-09-19 15:49:43 -0500

    underConstruction template

--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 print("Server started")
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello</p>"
+def hello_world(name=None):
+    return render_template('main.html', name=name)
 
 
