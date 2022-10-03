8327ad08b659bfcc04adc4c9c401fffe8fab0a60
TektiticEel9062 <luismagpesc@gmail.com>
2022-10-03 15:50:36 -0500

    Materiales Hard

--- webDesign/listos/materialesHard.js
+++ webDesign/listos/materialesHard.js
@@ -1,18 +1,17 @@
 function add_div(){
     var item_list = ["imac 24", "imac 27", "imac 32", "imac 36", "imac 40", "imac 44", "imac 48", "imac 52", "imac 56", "imac 60"];
     var descripcion_list = ["Computadora iMac de 24 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 27 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 32 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 36 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 40 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 44 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 48 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 52 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 56 con procesador M1 y sistema operativo MacOs Monterey", "Computadora iMac de 60 con procesador M1 y sistema operativo MacOs Monterey"];
-
+    var sistema_list = ["windows", "windows", "windows", "windows", "windows", "windows", "windows", "windows", "windows", "windows"];
+    var prefijos_list = ["PC-01", "PC-02", "PC-03", "PC-04", "PC-05", "PC-06", "PC-07", "PC-08", "PC-09", "PC-10"];
     for(let i = 0; i < 10; i++){
             let the = `<div class="single_row_user" id="${item_list[i]}">
-
+      
+                        <p id="prefijo">${prefijos_list[i]}</p>
                         <p id="nombreObjeto">${item_list[i]}</p>
                         <p id="descripcionObjeto">${descripcion_list[i]}</p>
                         <input type="number" name="cantidad" id="cantidad" class="numero">
-                        <select name="tipo" id="tipo">
-                            <option value="1">Hardware</option>
-                            <option value="2">Software</option>
-                            <option value="3">Espacio</option>
-                        </select>
+                        <p id="tipo"> Hardware </p>
+                        <p id="tipo">${sistema_list[i]}</p>
                         <div class="checkbox">
                                     <input type="checkbox" name="disponible" id="disponible" class="checkbox">
                         </div>
@@ -30,18 +29,22 @@ function add_div(){
 function new_div(obj){
 
     for(let i = 0; i < 10; i++){
-            let the =`<div class="single_row_user" id="${item_list[i]}">
-
-        
-            
+            let the = `<div class="single_row_user" id="${item_list[i]}">
+      
+            <p id="prefijo">${prefijos_list[i]}</p>
+            <p id="nombreObjeto">${item_list[i]}</p>
+            <p id="descripcionObjeto">${descripcion_list[i]}</p>
+            <input type="number" name="cantidad" id="cantidad" class="numero">
+            <p id="tipo"> Hardware </p>
+            <p id="tipo">${sistema_list[i]}</p>
             <div class="checkbox">
                         <input type="checkbox" name="disponible" id="disponible" class="checkbox">
             </div>
             
-            <div><button id="row_block" onclick="block_button('${item_list[i]}');">Block</button></div>
             <div><button id="row_delete" onclick="delete_button('${item_list[i]}');">Delete</button></div>
             <div><button id="row_save" onclick="save_button('${item_list[i]}');">Save</button></div>
-        </div>`;
+        </div>
+        <br>`;
         $('.div_list_users').append(the);
         console.log("lol");
     }
@@ -66,9 +69,4 @@ function delete_button(id_val){
 
 function save_button(id_val){
     alert("saved " + id_val)
-}
-
-
-function block_button(id_val){
-    alert("blocked " + id_val)
 }
