eebe04ab610bfb7507f670e29c097e0ad9671e93
TektiticEel9062 <luismagpesc@gmail.com>
2022-10-03 21:55:15 -0500

    Ajax for reg

--- webDesign/listos/reg.js
+++ webDesign/listos/reg.js
@@ -21,81 +21,65 @@
             var contrasena2 = $("#password2").val();
 
 
-            if (nombre == "" ) {
-                $("#name").focus();
-                $("#nameid").css("color", "red");
-                return false;
-            } else if (!name1.test(nombre)) {
-                $("#name").focus();
-                $("#nameid").css("color", "red");
-                return false;
-            } else if (apellido == "") {
-                $("#surname").focus();
-                $("#surnameid").css("color", "red");
-                return false;
-            } else if (!surname.test(apellido)) {
-                $("#surname").focus();
-                $("#surnameid").css("color", "red");
-                return false;
-            } else if (usuario == "") {
-                $("#username").focus();
-                $("#usernameid").css("color", "red");
-                return false;
-            } else if (!username.test(usuario)) {
-                $("#username").focus();
-                $("#usernameid").css("color", "red");
-                return false;
-            } else if (birthdate == "dd/mm/aaaa") {
-                $("#birth").focus();
-                $("#birthid").css("color", "red");
-                return false;
-            } else if (organizacion == "") {
-                $("#organization").focus();
-                $("#organizationid").css("color", "red");
-                return false;
-            } else if (correo == "") {
-                $("#email").focus();
-                $("#emailid").css("color", "red");
-                return false;
-            } else if (!email.test(correo)) {
-                $("#email").focus();
-                $("#emailid").css("color", "red");
-                return false;
-           //} else if (!emailTec.test(correo)) {
-                
-             //   $("#email").focus();
-               // return false;
-            } else if (age1 == "") {
-                $("#age").focus();
-                $("#ageid").css("color", "red");
-                return false;
-            } else if (!age1.test(age)) {
-                $("#age").focus();
-                $("#ageid").css("color", "red");
-                return false;
-            } else if (contrasena == "") {
-                $("#password").focus();
-                $("#passwordid").css("color", "red");
-                return false;
-            } else if (!password.test(contrasena)) {
-                $("#password").focus();
-                $("#passwordid").css("color", "red");
-                return false;
-            } else if (contrasena2 == "") {
-                $("#password2").focus();
-                $("#passwordid2").css("color", "red");
-                return false;
-            } else if (contrasena != contrasena2) {
-                $("#password2").focus();
-                $("#passwordid2").css("color", "red");
-                return false;
-            } else if (contrasena == contrasena2) {
-                hash256();
-                return true;
-            } else {
-                return true;
-            }  
-        })});
+            switch (true) {
+                case (nombre == ""):
+                    $("#name").focus();
+                    $("#nameid").css("color", "red");
+                case (!name1.test(nombre)):
+                    $("#name").focus();
+                    $("#nameid").css("color", "red");
+                case (apellido == ""):
+                    $("#surname").focus();
+                    $("#surnameid").css("color", "red");
+                case (!surname.test(apellido)):
+                    $("#surname").focus();
+                    $("#surnameid").css("color", "red");
+                case (usuario == ""):
+                    $("#username").focus();
+                    $("#usernameid").css("color", "red");
+                case (!username.test(usuario)):
+                    $("#username").focus();
+                    $("#usernameid").css("color", "red");
+                case (birthdate == ""):
+                    $("#birthdate").focus();
+                    $("#birthdateid").css("color", "red");
+                case (organizacion == ""):
+                    $("#organization").focus();
+                    $("#organizationid").css("color", "red");
+                case (correo == ""):
+                    $("#email").focus();
+                    $("#emailid").css("color", "red");
+                case (!email.test(correo)):
+                    $("#email").focus();
+                    $("#emailid").css("color", "red");
+                case (age == ""):
+                    $("#age").focus();
+                    $("#ageid").css("color", "red");
+                case (!age1.test(age)):
+                    $("#age").focus();
+                    $("#ageid").css("color", "red");
+                case (contrasena == ""):
+                    $("#password").focus();
+                    $("#passwordid").css("color", "red");
+                case (!password.test(contrasena)):
+                    $("#password").focus();
+                    $("#passwordid").css("color", "red");
+                case (contrasena2 == ""):
+                    $("#password2").focus();
+                    $("#password2id").css("color", "red");
+                case (!password.test(contrasena2)):
+                    $("#password2").focus();
+                    $("#password2id").css("color", "red");
+                case (contrasena != contrasena2):
+                    $("#password2").focus();
+                    $("#password2id").css("color", "red");
+                default:
+                    return true;
+
+           
+            }
+        });
+    });
 
 $(document).ready(function () {
     $("#formulario").on("input", function () {
@@ -115,10 +99,73 @@ $(document).ready(function () {
 // hash256 
 
 function hash256() {
-    var hash = CryptoJS.SHA256($("#password").val());
+    var hash = SHA256($("#password").val());
     $("#password").val(hash);
+
+    console.log(hash);
 
 }
 
-//navbar 
+//ajax 
 
+
+/*
+@app.route("/api/register")
+def register():
+    body = request.get_json()
+    cur = get_db().cursor()
+    emailSearch = cur.execute("SELECT Users.userId FROM Users WHERE email = ?", (body["email"],))
+    usernameSearch = cur.execute("SELECT Users.userId FROM Users WHERE username = ?", (body["username"],))
+    if emailSearch is not None:
+        respBody = json.dumps({"registered":False, "errorId":110})#, "desc":"Email is already registered"
+    elif usernameSearch is not None:
+        respBody = json.dumps({"registered":False, "errorId":111})#, "desc":"Username is already registered"
+    else:
+        
+        cur.execute('''INSERT INTO "main"."Users"
+                       ("dateRegistered", "firstName", "lastName", "username", "birthDate", "organization", 
+                       "email", "ocupation", "countryId", "hashPassword")
+                       VALUES ('?', '?', '?', '?', '?', '?', '?', '?', ?, '?');''',
+                       (datetime.now(timezone(-timedelta(hours=5))).strftime("%Y-%m-%d %H:%M:%S"), body["firstName"], body["lastName"], body["username"],
+                       body["birthDate"], body["organization"], body["email"], body["ocupation"], body["countryId"], body["hashPassword"]))
+        respBody = json.dumps({"registered":True})*/
+
+$(document).ready(function () {
+    $("#botonenvio").click(function () {
+        var nombre = $("#name").val();
+        var apellido = $("#surname").val();
+        var usuario = $("#username").val();
+        var birthdate = $("#birthdate").val();
+        var organizacion = $("#organization").val();
+        var correo = $("#email").val();
+        var age = $("#age").val();
+        var contrasena = $("#password").val();
+        var contrasena2 = $("#password2").val();
+
+        var data = {
+            "firstName": nombre,
+            "lastName": apellido,
+            "username": usuario,
+            "birthDate": birthdate,
+            "organization": organizacion,
+            "email": correo,
+            "ocupation": age,
+            "countryId": 1,
+            "hashPassword": contrasena
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5000/api/register",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+            },
+            failure: function (errMsg) {
+                alert(errMsg);
+            }
+        });
+    });
+});
