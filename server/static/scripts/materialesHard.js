6116d29a29ec064d17dbae52174a116ef66bf04d
TektiticEel9062 <luismagpesc@gmail.com>
2022-10-05 18:31:23 -0500

    delete y save materiales hard

--- server/static/scripts/materialesHard.js
+++ server/static/scripts/materialesHard.js
@@ -6,9 +6,9 @@ for (var i = 0; i < hardW.length; i++) {
     var hardwarePrefix = hardware.prefix;
     var hardwareMaxDays = hardware.maxDays;
     var hardwareQuantity = hardware.quantity;
-    var hardwareID = hardware.hardwareId;
+    var hardwareID = hardware.classId;
 
-    let the = `<div class="single_row_user" id="`+ hardwareName +`">
+    let the = `<div class="single_row_user" id="`+ hardwareID +`">
       
                         <p id="prefijo">`+ hardwarePrefix +`</p>
                         <p id="nombreObjeto">`+ hardwareName +`</p>
@@ -20,8 +20,8 @@ for (var i = 0; i < hardW.length; i++) {
                                     <input type="checkbox" name="disponible" id="disponible" class="checkbox">
                         </div>
                         
-                        <div><button id="row_delete" onclick="delete_button('`+ hardwareName +`');">Delete</button></div>
-                        <div><button id="row_save" onclick="save_button('`+ hardwareName +`s');">Save</button></div>
+                        <div><button id="row_delete" onclick="delete_button('`+ hardwareID +`');">Delete</button></div>
+                        <div><button id="row_save" onclick="save_button('`+ hardwareID +`s');">Save</button></div>
                     </div>
                     <br>`;
         $('.div_list_users').append(the);
@@ -43,13 +43,31 @@ function delete_button(id_val){
         easing: "easeInOutCubic"
     });
     setTimeout(move_rows, 800, id_val);
+    $.ajax({ 
+        url: 'api/edit/hardware', //cambiar esto por la ruta del servidor y añadir bien el json
+        type: 'POST',
+        data: JSON.stringify({ "classId" : hardwareID }),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data){
+            alert(data);
+        }
+    
+        });
 }
 
 function save_button(id_val){
     alert("saved " + id_val)
+    $.ajax({ 
+    url: 'api/edit/hardware', //cambiar esto por la ruta del servidor y añadir bien el json
+    type: 'POST',
+    data: JSON.stringify({ "name" : hardwareName, "description" : hardwareDescription, "prefix" : hardwarePrefix, "maxDays" : hardwareMaxDays, "quantity" : hardwareQuantity, "operativeSystem" : hardwareOS, "classId" : hardwareID }),
+    contentType: "application/json",
+    dataType: "json",
+    success: function(data){
+        alert(data);
+    }
+
+    });
 }
 
-//ajax para guardar en la base de datos
-function save_all(){
-    alert("saved all")
-}
