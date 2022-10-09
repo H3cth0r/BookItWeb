020695c9ef647f424a6ff9d924cb91b73d85fdfd
Victor P <victor32154@gmail.com>
2022-10-09 00:41:09 -0500

    Added mail functionality + updated sql file

--- server/DB/db.sql
+++ server/DB/db.sql
@@ -277,6 +277,14 @@ INSERT INTO HardwareObjects VALUES(20,3,1);
 INSERT INTO HardwareObjects VALUES(21,3,2);
 INSERT INTO HardwareObjects VALUES(22,3,3);
 INSERT INTO HardwareObjects VALUES(23,4,1);
+INSERT INTO HardwareObjects VALUES(24,5,1);
+INSERT INTO HardwareObjects VALUES(25,5,2);
+INSERT INTO HardwareObjects VALUES(26,5,3);
+INSERT INTO HardwareObjects VALUES(27,6,1);
+INSERT INTO HardwareObjects VALUES(28,6,2);
+INSERT INTO HardwareObjects VALUES(29,6,3);
+INSERT INTO HardwareObjects VALUES(30,6,4);
+INSERT INTO HardwareObjects VALUES(31,6,5);
 CREATE TABLE IF NOT EXISTS "SoftwareObjects" (
 	"inTypeId"	INTEGER NOT NULL,
 	"classId"	INTEGER NOT NULL,
@@ -315,6 +323,8 @@ INSERT INTO SoftwareObjects VALUES(41,6,2);
 INSERT INTO SoftwareObjects VALUES(42,6,3);
 INSERT INTO SoftwareObjects VALUES(43,6,4);
 INSERT INTO SoftwareObjects VALUES(44,6,5);
+INSERT INTO SoftwareObjects VALUES(45,7,1);
+INSERT INTO SoftwareObjects VALUES(46,7,2);
 CREATE TABLE IF NOT EXISTS "RegisteredChanges" (
 	"id"	INTEGER NOT NULL,
 	"userId"	INTEGER NOT NULL,
@@ -385,6 +395,18 @@ INSERT INTO AvailableObjects VALUES(69,NULL,41,NULL);
 INSERT INTO AvailableObjects VALUES(70,NULL,42,NULL);
 INSERT INTO AvailableObjects VALUES(71,NULL,43,NULL);
 INSERT INTO AvailableObjects VALUES(72,NULL,44,NULL);
+INSERT INTO AvailableObjects VALUES(73,24,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(74,25,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(75,26,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(76,27,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(77,28,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(78,29,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(79,30,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(80,31,NULL,NULL);
+INSERT INTO AvailableObjects VALUES(81,NULL,45,NULL);
+INSERT INTO AvailableObjects VALUES(82,NULL,46,NULL);
+INSERT INTO AvailableObjects VALUES(83,NULL,NULL,11);
+INSERT INTO AvailableObjects VALUES(84,NULL,NULL,12);
 CREATE TABLE IF NOT EXISTS "HardwareClass" (
 	"classId"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
@@ -400,6 +422,8 @@ INSERT INTO HardwareClass VALUES(1,'Mac Book Air','macOS 12',replace('CPU = M1\n
 INSERT INTO HardwareClass VALUES(2,'DELL PC','Windows 10',replace(replace('CPU = i5\r\nRAM = 8GB\r\nSSD = 256GB','\r',char(13)),'\n',char(10)),'DLPCW10',1,10,0);
 INSERT INTO HardwareClass VALUES(3,'iPhone 11','iOS 12',replace('Núcleos = 4\nRAM = 6GB\nSSD = 64GB','\n',char(10)),'IPHONE11',1,15,0);
 INSERT INTO HardwareClass VALUES(4,'Arduino','Arduino','Arduino 0','ARD',1,1,1);
+INSERT INTO HardwareClass VALUES(5,'Raspberry Pi 4 B','Raspberry Pi OS',replace('USB 2 = 2\nUSB 3 = 2\nRAM = 4GB\nMicro HDMI = 2','\n',char(10)),'RSPPI4B',1,1,0);
+INSERT INTO HardwareClass VALUES(6,'Cisco 4331','Cisco iOS',replace('Gigabit = 2\nEspacios de Módulo = 2','\n',char(10)),'CSRT4331',1,1,0);
 CREATE TABLE IF NOT EXISTS "SoftwareClass" (
 	"classId"	INTEGER NOT NULL,
 	"brand"	TEXT NOT NULL,
@@ -418,6 +442,7 @@ INSERT INTO SoftwareClass VALUES(3,'Autodesk','Autodesk Maya','Autodesk Maya es 
 INSERT INTO SoftwareClass VALUES(4,'Minitab Inc.','Minitab','Minitab es un programa de computadora diseñado para ejecutar funciones estadísticas básicas y avanzadas.','Windows 7+','MNTB',0,10,0);
 INSERT INTO SoftwareClass VALUES(5,'Adobe','Adobe Photoshop','Adobe XD apoya al diseño vectorial y a los sitios web wireframe, creando prototipos simples e interactivos con un solo clic.','Windows 10+','ADBXDW10',0,12,0);
 INSERT INTO SoftwareClass VALUES(6,'14','Clip Studio Paint','Clip Studio Paint es una aplicación de ilustración para Mac OS X y Microsoft Windows desarrollado por Celsys para la creación digital de cómics.','Windows 7+','CSPW7',1,30,0);
+INSERT INTO SoftwareClass VALUES(7,'Pixar','RenderMan','RenderMan es tanto un software como una API desarrollada por Pixar para redes de renderizado distribuido de complejas imágenes tridimensionales, el cual es empleado en granjas de renderizado con muchos equipos cliente.','Windows 10+','RNDRMN',1,1,0);
 CREATE TABLE IF NOT EXISTS "Rooms" (
 	"roomId"	INTEGER NOT NULL,
 	"label"	TEXT NOT NULL,
@@ -439,6 +464,8 @@ INSERT INTO Rooms VALUES(7,'SC03','Sala de Conferencias 03','Hub de Cibersegurid
 INSERT INTO Rooms VALUES(8,'SC04','Sala de Conferencias 04','Hub de Ciberseguridad, piso 3.','Sala de conferencias, apta para presentaciones ejecutivas a un público grande.',35,1,12,0);
 INSERT INTO Rooms VALUES(9,'1','123','123','123',123,1,123,0);
 INSERT INTO Rooms VALUES(10,'asfd a','aaaa','asfdas ','asdf as daSD ',34,0,234,0);
+INSERT INTO Rooms VALUES(11,'ADG','Auditorio General','Hub de Ciberseguridad, planta baja.','Auditorio ideal para conferencias a un público grande.',200,1,1,0);
+INSERT INTO Rooms VALUES(12,'ADG2','Auditorio General 2','Hub de Ciberseguridad, planta baja.','Auditorio ideal para conferencias a un público grande.',200,1,1,0);
 CREATE TABLE IF NOT EXISTS "ReservationTicket" (
 	"ticketId"	INTEGER NOT NULL,
 	"dateRegistered"	TEXT NOT NULL,
@@ -467,7 +494,7 @@ INSERT INTO ReservationTicket VALUES(23,'2022-10-04 16:41:25.596',4,'HRDWR','DEL
 INSERT INTO ReservationTicket VALUES(24,'2022-10-04 16:41:38.448',4,'HRDWR','DELL PC','2022-10-20 22:00:00.000','2022-10-21 12:00:00.000',3,'Reserva Dell',13.999999999999999999,'a8db3050f1');
 INSERT INTO ReservationTicket VALUES(25,'2022-10-04 17:33:53.547',4,'HRDWR','DELL PC','2022-10-22 12:00:00.000','2022-10-23 12:00:00.000',3,'Reserva Dell',24.0,'e3552bb97a');
 INSERT INTO ReservationTicket VALUES(26,'2022-10-04 17:36:07.656',4,'HRDWR','DELL PC','2022-10-24 12:00:00.000','2022-10-24 22:00:00.000',1,'Reserva Dell',10.0,'b7d97a737e');
-INSERT INTO ReservationTicket VALUES(27,'2022-10-06 12:42:26.000',28,'HRDWR','DELL PC','2022-10-06 16:00:0.000','2022-10-06 18:00:0.000',3,'Reserva pc dell',2.0,'123456789a');
+INSERT INTO ReservationTicket VALUES(27,'2022-10-06 12:42:26.000',28,'HRDWR','DELL PC','2022-10-06 16:00:00.000','2022-10-06 18:00:00.000',1,'Reserva pc dell',2.0,'123456789a');
 CREATE TABLE IF NOT EXISTS "Users" (
 	"userId"	INTEGER NOT NULL,
 	"dateRegistered"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -490,15 +517,33 @@ INSERT INTO Users VALUES(1,'2022-09-28 17:57:41.000','Pepo','Lopez','pepo117','2
 INSERT INTO Users VALUES(3,'2022-10-04 16:38:01.000','Victor','Portilla','nonwiz','2002-11-01 00:00:00.000','Tec','a01659198@tec.mx','Estudiante',107,'5f77c2500f56fe1a4abe06bf961012a3ea513ce8fbbbf4fec4d58339f95630d9',0,0,0);
 INSERT INTO Users VALUES(4,'2022-10-05 11:28:53','Yael','Goan','yael.goan01','1995-11-30','UNAM','yael.goan01@gmail.com','Estudiante',120,'kirakira',1,0,0);
 INSERT INTO Users VALUES(5,'2022-10-05 11:32:13','Misael','Chavez','wada','2002-06-20','Tec','a01653495@tec.mx','Estudiante',1,'bussybussy',0,0,0);
+INSERT INTO Users VALUES(6,'2022-10-06 17:07:13','yoel','ledo','ledo','1995-01-01','Tec','ledo@tec.mx','Profesor',1,'asdfgasdfg',0,0,0);
+CREATE TABLE IF NOT EXISTS "ToVerify" (
+	"firstName"	TEXT NOT NULL,
+	"lastName"	TEXT NOT NULL,
+	"username"	TEXT NOT NULL,
+	"birthDate"	TEXT NOT NULL,
+	"organization"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL,
+	"ocupation"	TEXT NOT NULL,
+	"countryId"	INTEGER NOT NULL,
+	"hashPassword"	TEXT NOT NULL,
+	"hashKey"	TEXT NOT NULL,
+	"id"	INTEGER NOT NULL,
+	FOREIGN KEY("countryId") REFERENCES "Countries"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+INSERT INTO ToVerify VALUES('csdvcs','wdecwdcwsd','dfsvsd','dsfv','dsfv','dsfv','sdfv',1,'sdfvzc','asfvwerv',1);
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('Countries',252);
-INSERT INTO sqlite_sequence VALUES('HardwareObjects',23);
-INSERT INTO sqlite_sequence VALUES('SoftwareObjects',44);
+INSERT INTO sqlite_sequence VALUES('HardwareObjects',31);
+INSERT INTO sqlite_sequence VALUES('SoftwareObjects',46);
 INSERT INTO sqlite_sequence VALUES('RegisteredChanges',0);
-INSERT INTO sqlite_sequence VALUES('AvailableObjects',72);
-INSERT INTO sqlite_sequence VALUES('HardwareClass',4);
-INSERT INTO sqlite_sequence VALUES('SoftwareClass',6);
-INSERT INTO sqlite_sequence VALUES('Rooms',10);
+INSERT INTO sqlite_sequence VALUES('AvailableObjects',84);
+INSERT INTO sqlite_sequence VALUES('HardwareClass',6);
+INSERT INTO sqlite_sequence VALUES('SoftwareClass',7);
+INSERT INTO sqlite_sequence VALUES('Rooms',12);
 INSERT INTO sqlite_sequence VALUES('ReservationTicket',27);
-INSERT INTO sqlite_sequence VALUES('Users',5);
+INSERT INTO sqlite_sequence VALUES('Users',10);
+INSERT INTO sqlite_sequence VALUES('ToVerify',1);
 COMMIT;
