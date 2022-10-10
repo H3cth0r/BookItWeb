5b7f5ac57b3f5d972f9457f9d9f44de342619e9e
Victor P <victor32154@gmail.com>
2022-10-09 21:43:03 -0500

    updated seleccionHardware

--- server/static/scripts/seleccionHardware.js
+++ server/static/scripts/seleccionHardware.js
@@ -1,28 +1,3 @@
-
-/*for (var i = 0; i < softW.length; i++) {
-    var software = softW[i];
-    var softwareName = software.name;
-    var softwareBrand = software.brand;
-    var softwareOS = software.operativeSystem;
-    var softwareDescription = software.description;
-    var softwarePrefix = software.prefix;
-    var softwareMaxDays = software.maxDays;
-    var softwareQuantity = software.quantity;
-    var softwareHTML = "<div class='software'>";
-    softwareHTML += "<h3>" + softwareName + "</h3>";
-    softwareHTML += "<p>" + softwareBrand + "</p>";
-    softwareHTML += "<p>" + softwareDescription + "</p>";
-    if (softwareQuantity > 0) {
-        softwareHTML += "<p>Disponible 🟢</p>";
-    } else {
-        softwareHTML += "<p>No disponible 🔴</p>";
-    }
-    
-    softwareHTML += "</div>";
-    
-    softwareHTML += "</div>";
-    document.write(softwareHTML);
-}*/
 $(document).ready(function () {
     for (var i = 0; i < hardw.length; i++) {
         var hardware = hardw[i];
